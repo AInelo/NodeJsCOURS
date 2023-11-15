8f1f4d44db579d97e890f46e45bb15b0cd7b672a
TOTON Lionel <totonlionel@gmail.com>
2023-11-15 22:29:21 +0100

    Le cours de Node Js suivi
    ICI j'ai appris à mettre en place un serveur de scratch

---
+++ 05TUT/contenueDuCours.md
@@ -0,0 +1,10 @@
+## CONTENU 
+ 
+ Ici on va concervoire un serveur avec NodeJs
+
+# const server = http.createServer((req, res) => {
+ #   console.log(req.url, req.method);
+# });
+ 
+ Dans cette commande, req = requête et res = réponse 
+
---
+++ 05TUT/server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter { };
+// Initialisons l'objet
+const myEmitter = new Emitter();
+
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+
+});
+
+server.listen(PORT, () => console.log(`Le serveur tourne sur ${PORT}`));
+
+
+
+
+
+
+
+// myEmitter.on('log', (msg) => logEvents(msg));
+
+//     myEmitter.emit('log', 'Log event emitted');
+
