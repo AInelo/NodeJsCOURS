f6bb35cc8df00a167315e1dbf9cdb52adedbc671
TOTON Lionel <totonlionel@gmail.com>
2023-11-15 03:23:53 +0100

    Voici le cous de NodeJs
    le cours est découpé en plusieurs sous étapes

--- 02TUT/index.js
+++ 02TUT/index.js
@@ -15,12 +15,20 @@ console.log('Hello.....')
 
 fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you', (err) => {
     if (err) throw err;
-    console.log('Write complete')
-})
+    console.log('Write complete');
 
-fs.appendFile(path.join(__dirname, 'files', 'test.txt'), 'Testing test', (err) => {
-    if (err) throw err;
-    console.log('Append complete')
+
+    fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nYes it is', (err) => {
+        if (err) throw err;
+        console.log('Append complete')
+
+
+
+        fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt'), '\n\nYes it is new', (err) => {
+            if (err) throw err;
+            console.log('Rename complete')
+        })
+    })
 })
 
 
