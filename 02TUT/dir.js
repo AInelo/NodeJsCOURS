562c9985e0e22483077e374db3c3b9d156bcb65f
TOTON Lionel <totonlionel@gmail.com>
2023-11-15 14:41:37 +0100

    Le cours de Node Js suivi
    ICI j'ai appris à copier coller et lire des documents avec le Javascript

--- 02TUT/dir.js
+++ 02TUT/dir.js
@@ -1,6 +1,23 @@
+// LA LIBRAIRIE POUR FAIRE DE MANIPULATION DE FICHIER
 const fs = require('fs');
 
-fs.mkdir('./new', (err) => {
-    if (err) throw err;
-    console.log('Directory created')
-}) 
+
+// CODE POUR CREER UN REPERTOIRE DANS DEUX CAS 
+// LE CAS OU IL EXITE DEJA, IL NE LE CREER PLUS
+
+if (!fs.existsSync('./new')) {
+    fs.mkdir('./new', (err) => {
+        if (err) throw err;
+        console.log('Directory created')
+    });
+}
+
+// CODE POUR SUPPRIMER UN REPERTOIRE
+
+// if (fs.existsSync('./new')) {
+//     fs.rmdir('./new', (err) => {
+//         if (err) throw err;
+//         console.log('Directory REMOVED')
+//     });
+// }
+
