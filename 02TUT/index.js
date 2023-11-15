dc36729effacca68b8cac7772c319d08d9708666
TOTON Lionel <totonlionel@gmail.com>
2023-11-15 03:16:52 +0100

    Voici le cous de NodeJs
    le cours est découpé en plusieurs sous étapes

--- 02TUT/index.js
+++ 02TUT/index.js
@@ -18,6 +18,11 @@ fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you', (er
     console.log('Write complete')
 })
 
+fs.appendFile(path.join(__dirname, 'files', 'test.txt'), 'Testing test', (err) => {
+    if (err) throw err;
+    console.log('Append complete')
+})
+
 
 // exit on uncaught errors
 process.on('uncaughtException', err => {
