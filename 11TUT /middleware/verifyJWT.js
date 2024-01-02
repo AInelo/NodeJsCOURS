78a754459fc98be93b9e75cf5414996173d8cc72
TOTON Lionel <totonlionel@gmail.com>
2024-01-02 23:01:36 +0100

    Le cours 11

--- 11TUT /middleware/verifyJWT.js
+++ 11TUT /middleware/verifyJWT.js
@@ -1,8 +1,25 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+// const verifyJWT = (req, res, next) => {
+//     const authHeader = req.hearders['authorization'];
+//     if (!authHeader) return res.sendStatus(401);
+//     console.log(authHeader); // Bearer token
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(
+//         token,
+//         process.env.ACCESS_TOKEN_SECRET,
+//         (err, decoded) => {
+//             if(err) return res.sendStatus(403); // invalid token
+//             req.user = decoded.username;
+//             next();
+//         } 
+//     )
+// }
+
+
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.hearders['authorization'];
+    const authHeader = req.headers['authorization'];
     if (!authHeader) return res.sendStatus(401);
     console.log(authHeader); // Bearer token
     const token = authHeader.split(' ')[1];
@@ -10,9 +27,11 @@ const verifyJWT = (req, res, next) => {
         token,
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
-            if(err) return res.sendStatus(403); // invalid token
+            if (err) return res.sendStatus(403); //invalid token
             req.user = decoded.username;
             next();
-        } 
-    )
+        }
+    );
 }
+
+module.exports = verifyJWT
