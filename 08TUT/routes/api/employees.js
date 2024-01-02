49c7c325de248ee86bd0506cef993504a9781820
TOTON Lionel <totonlionel@gmail.com>
2024-01-02 14:54:51 +0100

    Reviews du cours 08

--- 08TUT/routes/api/employees.js
+++ 08TUT/routes/api/employees.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const router = express.Router();
-// const path = require('path');
+const path = require('path');
+/* data.employees = require('../../data/employees.json'); */
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+
 const data = {
     employees: []
 };
-/* data.employees = require('../../data/employees.json'); */
 
-const bodyParser = require('body-parser');
-const fs = require('fs');
-
-const app = express();
-
-
-app.use(bodyParser.json());
+router.use(bodyParser.json());
 
 // Load data from the file initially
-fs.readFile('../../data/employees.json', 'utf8', (err, jsonString) => {
+const filePath = path.join(__dirname, '../../data/employees.json');
+fs.readFile(filePath, 'utf8', (err, jsonString) => {
     if (err) {
-      console.log('Error reading file:', err);
-      return;
+        console.log('Error reading file:', err);
+        return;
     }
     try {
-      data.employees = JSON.parse(jsonString);
+        data.employees = JSON.parse(jsonString);
     } catch (err) {
-      console.log('Error parsing JSON:', err);
+        console.log('Error parsing JSON:', err);
     }
-  });
+});
+
 
 router.route('/')
     .get((req, res) => {
@@ -36,23 +36,22 @@ router.route('/')
         //     "firstname": req.body.firstname,
         //     "lastname": req.body.lastname
         // });
-
         // Add the new employee to the data
-        const newEmployee = {
+      const newEmployee = {
+        "id": req.body.id,
         "firstname": req.body.firstname,
         "lastname": req.body.lastname
       };
       data.employees.push(newEmployee);
 
       // Update the employees.json file
-      fs.writeFile('../../data/employees.json', JSON.stringify(data, null, 2), (err) => {
+      fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
         if (err) {
-          console.log('Error writing file:', err);
-          return res.status(500).json({ error: 'Internal Server Error' });
+            console.log('Error writing file:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
         }
         res.json(newEmployee);
-      });
-
+        });
     })
     .put((req, res) => {
         res.json({
