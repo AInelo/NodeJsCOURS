880972ec229af7643b66fa238d76eb556e0e1f6f
TOTON Lionel <totonlionel@gmail.com>
2024-01-02 21:03:35 +0100

    Le cours 11

--- 11TUT /contenueDuCours.md
+++ 11TUT /contenueDuCours.md
@@ -3,5 +3,13 @@
 
 ## Pour encrypter les mots de passe 
 ```bash
-npm i bcrypt
+npm i dotenv jsonwebtoken cookie-parser
+```
+## Pour avoir un TOKEN
+```bash
+node
+```
+
+```bash
+require('crypto').randomBytes(64).toString('hex')
 ```
--- 11TUT /controllers/authController.js
+++ 11TUT /controllers/authController.js
@@ -5,6 +5,10 @@ const userDB = {
     }
 }
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+
 
 const handleLogin = async (req, res) => {
     const {user, pwd} = req.body;
@@ -17,6 +21,19 @@ const handleLogin = async (req, res) => {
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
         // create JWTs
+
+        const accessToken = jwt.sign(
+            {"usename": foundUser.username },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '30s' }
+        );
+        const refreshToken = jwt.sign(
+            {"usename": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+
+    
         res.json({'sucess': `User ${user} is logged in!`});
     } else {
         res.sendStatus(401);
