07eda98e58e7937219e15a371b14f2c6a23f8f27
TOTON Lionel <totonlionel@gmail.com>
2024-01-04 00:57:32 +0100

    Le cours 12

--- 12TUT/server.js
+++ 12TUT/server.js
@@ -14,8 +14,6 @@ const PORT = process.env.PORT || 3500;
 // custom middleware logger
 app.use(logger);
 
-
-
 // Handle options credentials check - before CORS!
 // and fetch cookies credentials requirement
 app.use(credentials);
@@ -41,6 +39,7 @@ app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
 app.use('/refresh', require('./routes/refresh'));
 app.use('/logout', require('./routes/logout'));
+
 
 app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));
