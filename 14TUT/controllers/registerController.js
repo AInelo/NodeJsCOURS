6932c63435e0c30866701be5281ce3747e8b5b59
TOTON Lionel <totonlionel@gmail.com>
2024-01-08 00:47:42 +0100

    Le cours 12

--- 14TUT/controllers/registerController.js
+++ 14TUT/controllers/registerController.js
@@ -8,20 +8,23 @@ const handleNewUser = async (req, res) => {
     const duplicate = await User.findOne({username: user}).exec();
 
     if (duplicate) return res.sendStatus(409); //Conflict 
-    try {
-        //encrypt the password
-        const hashedPwd = await bcrypt.hash(pwd, 10);
-        //store the new user
-        const newUser = {
-            "username": user,
-            "roles": { "User": 2001 },
-            "password": hashedPwd
-        };
-        
-        res.status(201).json({ 'success': `New user ${user} created!` });
-    } catch (err) {
-        res.status(500).json({ 'message': err.message });
-    }
+   
+        try {
+            //encrypt the password
+            const hashedPwd = await bcrypt.hash(pwd, 10);
+
+            //create and store the new user
+            const result = await User.create({
+                "username": user,
+                "password": hashedPwd
+            });
+
+            console.log(result);
+
+            res.status(201).json({ 'success': `New user ${user} created!` });
+        } catch (err) {
+            res.status(500).json({ 'message': err.message });
+        }
 }
 
 
--- 14TUT/model/User.js
+++ 14TUT/model/User.js
@@ -1,26 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    firstname: {
+    username: {
         type: String,
         required: true
     },
     roles: {
         User: {
-            type: number,
+            type: Number,
             default: 2001
-        }, 
+        },
         Editor: Number,
         Admin: Number
     },
-
-
     password: {
         type: String,
         required: true
     },
     refreshToken: String
-})
+});
 
 module.exports = mongoose.model('User', userSchema);
